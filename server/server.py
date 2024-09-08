01b054ed4b2c1d0d5a492efaa092bb429298c062
Satyapranay1 <satyapranay114@gmail.com>
2024-09-08 15:30:29 +0530

    first commit

---
+++ server/server.py
@@ -0,0 +1,9 @@
+import pygame
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((640,480))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == p
